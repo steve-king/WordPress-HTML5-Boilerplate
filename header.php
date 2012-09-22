9e1daf027ded6c4446fa132f6da6f304a51e0a78
Steve <steve.king5891@gmail.com>
2012-09-22 17:09:13 +0100

    Added lt-ie10 class, <article> tag, site header

--- header.php
+++ header.php
@@ -2,7 +2,8 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -10,15 +11,17 @@
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width">
 	
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_uri'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url	'); ?>">
 	
-	<?php wp_head(); ?>
+	<?php wp_head();?>
 </head>
-<body<?php body_class(); ?>>
+<body<?php body_class();?>>
+	
 	<!--[if lt IE 7]>
 		<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
 	<![endif]-->
     
 	<header id="header">
-    
+    	<h1><?php bloginfo('title');?></h1>
+    	<p><?php bloginfo('description');?></p>
 	</header>
--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 	<div id="main">
 		
 		<div class="main-col">
+			<article>
 			<?php if(have_posts()) : while(have_posts()): the_post(); ?>
 		    	
 		    	<h1><?php the_title(); ?></h1>
 		    	<?php the_content(); ?>
 			
 			<?php endwhile; endif; ?>
+			</article>
 		</div>
 		
 		<?php get_sidebar();?>
