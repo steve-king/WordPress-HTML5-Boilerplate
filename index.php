d76ba6d63e93540bc7018f1978ee692a05d92bb2
Steve <steve.king5891@gmail.com>
2012-09-22 17:17:00 +0100

    updated index.php

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php get_header();?>
+		
+<div class="main-col">
+	<article>
+	<?php if(have_posts()) : while(have_posts()): the_post(); ?>
+    	
+    	<h1><?php the_title(); ?></h1>
+    	<?php the_content(); ?>
 	
-	<div id="main">
-		
-		<div class="main-col">
-			<article>
-			<?php if(have_posts()) : while(have_posts()): the_post(); ?>
-		    	
-		    	<h1><?php the_title(); ?></h1>
-		    	<?php the_content(); ?>
-			
-			<?php endwhile; endif; ?>
-			</article>
-		</div>
-		
-		<?php get_sidebar();?>
-		
-	</div>
-	
+	<?php endwhile; endif; ?>
+	</article>
+</div>
+
+<?php get_sidebar();?>
 <?php get_footer();?>    
